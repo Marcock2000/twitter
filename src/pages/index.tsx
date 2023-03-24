d497d17cefe4f17fbd8b91466d5caa97ee1fbbd3
Marcock2000 <92387883+Marcock2000@users.noreply.github.com>
2023-03-24 17:40:00 +0300

    Loading behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ dayjs.extend(relativeTime);
 import { api } from "~/utils/api";
 import type  {RouterOutputs}  from "~/utils/api";
 import Image from "next/image";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 
 const CreatePostWizard = () => {
 
@@ -48,16 +49,33 @@ const PostView = (props: PostWithUser) => {
   );
 }
 
-const Home: NextPage = () => {
-  const { data, isLoading } = api.posts.getAll.useQuery();
+const Feed = () => {
 
-  const user = useUser();
-  if (isLoading) {
-    return <div>Loading...</div>;
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+
+  if (postsLoading ) return <LoadingPage/>
+
+  if (!data) return <div> Something went wrong</div>
+return (
+  <div className="flex flex-col">
+  {[...data,...data]?.map((fullPost) => (
+    <PostView {...fullPost} key= {fullPost.post.id}/>))}
+</div>
+)
   }
-  if (!data) {
-    return <div>Something went wrong...</div>;
-  }
+  
+  // if both arent loaded
+
+
+const Home: NextPage = () => {
+  api.posts.getAll.useQuery();
+
+  const { isLoaded: userLoaded, isSignedIn} = useUser();
+  
+  // if both arent loaded 
+if (!userLoaded ) return <div/>
+
+
 
   return (
     <>
@@ -69,17 +87,17 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
       <div className="w-full h-full border-x border-slate-400 md:max-w-2xl">
         <div className= "flex border-b border-slate-400 p-4">
-        {!user.isSignedIn && (
+        {!isSignedIn && (
          <div className="flex justify-center">
           <SignInButton/>
           </div>
         )}
-        {user.isSignedIn && <CreatePostWizard/>}
+        {isSignedIn && (
+        <CreatePostWizard/>)
+         }
       </div> 
-      <div className="flex flex-col">
-        {[...data,...data]?.map((fullPost) => (
-          <PostView{...fullPost} key= {fullPost.post.id}/>))}
-      </div>
+      <Feed/>
+
       </div>
 
       </main>
